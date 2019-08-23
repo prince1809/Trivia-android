b0188554465e80ff690f9cfb411caa2ecdffda4b
prince <prince18pat@gmail.com>
2019-08-24 01:36:26 +0900

    add navigation and layout

--- app/src/main/java/com/princekr/android/trivia/MainActivity.kt
+++ app/src/main/java/com/princekr/android/trivia/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.princekr.android.trivia
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import com.princekr.android.trivia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        drawerLayout = binding.drawerLayout
+
     }
 }
